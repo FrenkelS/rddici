9592f312210d6886831a59bc626cd47f73cef3c9
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2026-01-04 01:00:22 +0100

    add RLE.C

---
+++ RLE.C
@@ -0,0 +1,19 @@
+void sub_0_5CC7(void)
+{
+asm {
+db 8Bh, 46h, 04h, 36h, 0A3h, 3Eh
+db 12h, 8Bh, 46h, 06h, 36h, 0A3h, 3Ah, 12h, 8Bh, 46h, 0Ch, 36h, 0A3h, 3Ch, 12h, 8Bh
+db 46h, 0Eh, 36h, 0A3h, 38h, 12h, 8Bh, 46h, 08h, 36h, 0A3h, 40h, 12h, 8Bh, 46h, 0Ah
+db 36h, 0A3h, 42h, 12h, 0E8h, 4Ch, 01h, 36h, 0A1h, 44h, 12h, 36h, 8Bh, 16h, 46h, 12h
+}
+}
+
+void sub_0_5D02(void)
+{
+asm {
+db 8Bh, 46h, 04h, 36h, 0A3h, 3Ch, 12h, 8Bh, 46h, 06h, 36h
+db 0A3h, 38h, 12h, 8Bh, 46h, 08h, 36h, 0A3h, 3Eh, 12h, 8Bh, 46h, 0Ah, 36h, 0A3h, 3Ah
+db 12h, 8Bh, 46h, 0Ch, 36h, 0A3h, 40h, 12h, 8Bh, 46h, 0Eh, 36h, 0A3h, 42h, 12h, 0E8h
+db 0A4h, 00h
+}
+}
