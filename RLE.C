c1bc7280c5e67b819cd745c43f16fe2a8f916239
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2026-05-20 21:46:39 +0200

    one instruction on one line

--- RLE.C
+++ RLE.C
@@ -20,10 +20,21 @@
 void sub_0_5CC7(void)
 {
 asm {
-db 8Bh, 46h, 04h, 36h, 0A3h, 3Eh
-db 12h, 8Bh, 46h, 06h, 36h, 0A3h, 3Ah, 12h, 8Bh, 46h, 0Ch, 36h, 0A3h, 3Ch, 12h, 8Bh
-db 46h, 0Eh, 36h, 0A3h, 38h, 12h, 8Bh, 46h, 08h, 36h, 0A3h, 40h, 12h, 8Bh, 46h, 0Ah
-db 36h, 0A3h, 42h, 12h, 0E8h, 4Ch, 01h, 36h, 0A1h, 44h, 12h, 36h, 8Bh, 16h, 46h, 12h
+db 8Bh, 46h, 04h
+db 36h, 0A3h, 3Eh, 12h
+db 8Bh, 46h, 06h
+db 36h, 0A3h, 3Ah, 12h
+db 8Bh, 46h, 0Ch
+db 36h, 0A3h, 3Ch, 12h
+db 8Bh, 46h, 0Eh
+db 36h, 0A3h, 38h, 12h
+db 8Bh, 46h, 08h
+db 36h, 0A3h, 40h, 12h
+db 8Bh, 46h, 0Ah
+db 36h, 0A3h, 42h, 12h
+db 0E8h, 4Ch, 01h
+db 36h, 0A1h, 44h, 12h
+db 36h, 8Bh, 16h, 46h, 12h
 }
 }
 
@@ -31,9 +42,18 @@ db 36h, 0A3h, 42h, 12h, 0E8h, 4Ch, 01h, 36h, 0A1h, 44h, 12h, 36h, 8Bh, 16h, 46h,
 void sub_0_5D02(void)
 {
 asm {
-db 8Bh, 46h, 04h, 36h, 0A3h, 3Ch, 12h, 8Bh, 46h, 06h, 36h
-db 0A3h, 38h, 12h, 8Bh, 46h, 08h, 36h, 0A3h, 3Eh, 12h, 8Bh, 46h, 0Ah, 36h, 0A3h, 3Ah
-db 12h, 8Bh, 46h, 0Ch, 36h, 0A3h, 40h, 12h, 8Bh, 46h, 0Eh, 36h, 0A3h, 42h, 12h, 0E8h
-db 0A4h, 00h
+db 8Bh, 46h, 04h
+db 36h, 0A3h, 3Ch, 12h
+db 8Bh, 46h, 06h
+db 36h, 0A3h, 38h, 12h
+db 8Bh, 46h, 08h
+db 36h, 0A3h, 3Eh, 12h
+db 8Bh, 46h, 0Ah
+db 36h, 0A3h, 3Ah, 12h
+db 8Bh, 46h, 0Ch
+db 36h, 0A3h, 40h, 12h
+db 8Bh, 46h, 0Eh
+db 36h, 0A3h, 42h, 12h
+db 0E8h, 0A4h, 00h
 }
 }
